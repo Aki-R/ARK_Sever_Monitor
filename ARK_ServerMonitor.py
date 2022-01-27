a8e49854e6e22b561277a2c41a7f4c54bfeda3e6
Aki-R <akiakr.s@gmail.com>
2022-01-28 01:28:23 +0900

    First commit

---
+++ ARK_ServerMonitor.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+import subprocess
+
+# Configuration
+# Define command to start ARK server program
+ARK_START_COMMAND = '/home/akira/python/ARK_Server_Monitor/LostIsland_Start.sh'
+
+if __name__ == "__main__":
+    # Check all process at the time
+    result = subprocess.getoutput('top -n 1')
+    # Check if ARK is in working or not
+    arkinwork = result.find('Shoot')
+
+    if arkinwork == -1:
+        # initialize ARK server program
+        subprocess.run(['nohup'ARK_START_COMMAND])
+        print('ARK is restarting')
+    else:
+        print('ARK server is in working now.')
